156c0beab23faeb73118feaea34d2ae01176ec3a
FerriAchede <ferriyt38@gmail.com>
2024-09-26 16:44:55 +0200

    pintarGraella

--- php/src/4ratlla/4ratlla.php
+++ php/src/4ratlla/4ratlla.php
@@ -3,37 +3,31 @@
 
 <head>
     <meta charset="utf-8">
-    <title>L'Ofegat</title>
+    <title>4ratlla</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-    <table>
-	<tbody>
+    
     <?php
     require 'functions.php';
 
-    const ROWS = 6;
-    const COLUMNS = 7;
+    $tableGame = inicialitzarGraella();
+    pintarGraella($tableGame);
 
-    echo inicialitzarGraella(ROWS,COLUMNS);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $position = $_POST['pos'];
+    }
 
     ?>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
+
+   
     
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div>
-            <label for="numero">Letra:</label>
-            <input type="number" id="numero" name="numero" value="0" required min=1 max=7>
+            <label for="pos">Posición:</label>
+            <input type="number" id="pos" name="pos" value="0" required min=1 max=7>
         </div>
         <div>
             <button type="submit">Enviar</button>
--- php/src/4ratlla/functions.php
+++ php/src/4ratlla/functions.php
@@ -1,14 +1,45 @@
 <?php
-function inicialitzarGraella($rows, $columns){
-    $graella = "";
-
-    for ($i=0; $i < $rows; $i++) { 
-        $graella .= "<tr>";
-        for ($j=0; $j < $columns; $j++) { 
-            $graella .= "<td class='buid'></td>";
+const ROWS = 6;
+const COLUMNS = 7;
+const lastLine = "<tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>";
+        
+function inicialitzarGraella(){
+    $table = [];
+    for ($i=0; $i < ROWS; $i++) { 
+        $table[$i][] = $i;
+        for ($j=0; $j < COLUMNS; $j++) { 
+            $table[$i][$j] = $j;
         }
-        $graella .= "</tr>";
     }
-    return $graella;
+    return $table;
 }
+
+function pintarGraella($graella){
+    echo "<table>";
+    foreach ($graella as $number => $row) {
+        echo "<tr>";
+        foreach ($row as $num => $value) {
+            echo "<td class='buid'></td>";
+        }
+        echo "</tr>";
+    }
+
+    for ($i=0; $i < 1; $i++) { 
+        echo "<tr>";
+        for ($j=0; $j < COLUMNS; $j++) { 
+            echo "<td></td>";
+        }
+    }
+    echo "</table>"; 
+}
+
+
 ?>
